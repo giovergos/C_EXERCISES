0d1f5edeb6f7161b9607cdab24a839f94b454a56
Georgios Vergos <94325473+giovergos@users.noreply.github.com>
2021-12-16 22:44:58 +0200

    Update Quantity of Cans.c

--- Quantity of Cans.c
+++ Quantity of Cans.c
@@ -1,3 +1,8 @@
+/* N is the quantity of a soda's cans. Every can "i" has capacity Bi ml and includes Ai ml (Ai<=Bi). A program that reads an integer number N (2<=N<=100 000) and then N integer numbers Ai (0<=Ai<=10^9) and N integer numbers Bi (0<=Bi<=10^9) and shows:
+"Wrong Input" if one of the values is off limits
+"Yes" if the quantity of the soda is less (or equal) than the capacity of two random cans
+"No" if the quantity of the soda is more than every capacity of a couple of cans */
+
 #include <stdio.h>
 #include <math.h>
 #define max 100000
