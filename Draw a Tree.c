5d033eb0b3b9cad26191a722381cf9491ea53259
Georgios Vergos <94325473+giovergos@users.noreply.github.com>
2021-12-17 22:21:09 +0200

    Update Draw a Tree.c

--- Draw a Tree.c
+++ Draw a Tree.c
@@ -1,3 +1,6 @@
+/* A program that reads an integer number N (0<N<11) and shows a christmas tree with '*' and spaces. If the integer is off limit the program shows "Wrong Input" and ends. The tree has to be drawn:
+The program shows N lines with '*' in total. The first line includes 1 '*', the second 3 '*', the third 5 '*', etc. Before the '*' the program has to shows spaces so every line is centralized to the middle '*'. */
+
 #include <stdio.h>
 
 int main(void) {
