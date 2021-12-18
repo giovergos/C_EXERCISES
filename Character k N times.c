9e4cb9b1047e78c67a10bff9cdbb7cd6d092eb5b
Georgios Vergos <94325473+giovergos@users.noreply.github.com>
2021-12-18 02:20:01 +0200

    Update Character k N times.c

--- Character k N times.c
+++ Character k N times.c
@@ -1,3 +1,6 @@
+/* A program that reads an integer number N (N>0) and a character k ([a,z]) seperated by a whitespace character. If the integer or the character is off limits the program shows "Wrong Input" and ends.
+The program shows N times the character k and at the end a '.'. After five k characters the program lets a space character, unless no other k is going to be shown (so it just puts a '.'). */
+
 #include <stdio.h>
 
 int main(void) {
